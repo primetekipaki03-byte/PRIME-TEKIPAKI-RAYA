dcd21cec9a9a681d4f5dd68d278c581bd0417ff0
primetekipaki03-byte <prime.tekipaki03@gmail.com>
2026-08-18 14:11:47 +0700

    Update data.js

--- data.js
+++ data.js
@@ -89,6 +89,9 @@ MATERI_BY_LEVEL.N4[0][0].link = "n4_minggu1materi1.html";
 MATERI_BY_LEVEL.N4[0][1].title = "Minggu 1 · Materi 2";
 MATERI_BY_LEVEL.N4[0][1].link = "n4_minggu1materi2.html";
 
+MATERI_BY_LEVEL.N4[0][2].title = "Minggu 1 · Materi 3";
+MATERI_BY_LEVEL.N4[0][2].link = "n4_minggu1materi3.html";
+
 /* Materi 2, 3, 4 di Minggu 1 N4 masih placeholder —
    ganti title & link-nya kalau sudah siap, contoh:
 
